32a6eba31f13fdd7adc048548ff04f534f18bce7
mizumura <satoshi.mizumura1@persol.co.jp>
2019-09-09 18:18:54 +0900

    add test data

---
+++ src/main/resources/sql/test_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO example.artists (id, name, birth, website) VALUES (7, 'Skrillex', '1988-01-15', 'https://skrillex.com/');
+INSERT INTO example.artists (id, name, birth, website) VALUES (8, 'ZEDD', '1989-09-02', 'https://www.zedd.net/');
